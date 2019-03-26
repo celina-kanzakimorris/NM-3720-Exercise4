772aaa17939cac54c1891ee38d8715a5c7c8ec3b
Celina <celina.kanzakimorris@uleth.ca>
2019-03-25 23:02:19 -0600

    added rows and columns of images, and messed with the css

--- js/scripts.js
+++ js/scripts.js
@@ -13,9 +13,19 @@ function init(){
 				var picsum = JSON.parse(xhr.responseText);
 				console.log(picsum);
 
-				for (var x = 500; x < 550; x++){
-					document.getElementById("gallery").innerHTML += "<img src='https://picsum.photos/200/300?image=" + picsum[x].id +"'><p>" + picsum[x].author +"</p>"
+				for (var x = 10; x < 12; x++){
+					document.getElementById("gallery").innerHTML += "<img src='https://picsum.photos/200?image=" + picsum[x].id +"'><a href='" + picsum[x].author_url + "'><p>" + picsum[x].author + "</p></a><a href='" + picsum[x].post_url + "'><p>Original Post</p></a>"
 
+				}
+				for (var x = 13; x < 17; x++){
+					document.getElementById("gallery_col2").innerHTML += "<img src='https://picsum.photos/g/200?image=" + picsum[x].id +"'><a href='" + picsum[x].author_url + "'><p>" + picsum[x].author + "</p></a><a href='" + picsum[x].post_url + "'><p>Original Post</p></a>"
+
+				}
+				for (var x = 18; x < 20; x++){
+					document.getElementById("gallery_col3").innerHTML += "<img src='https://picsum.photos/200?image=" + picsum[x].id +"'><a href='" + picsum[x].author_url + "'><p>" + picsum[x].author + "</p></a><a href='" + picsum[x].post_url + "'><p>Original Post</p></a>"
+				}
+				for (var x = 22; x < 26; x++){
+					document.getElementById("gallery-col4").innerHTML += "<img src='https://picsum.photos/g/200?image=" + picsum[x].id +"'><a href='" + picsum[x].author_url + "'><p>" + picsum[x].author + "</p></a><a href='" + picsum[x].post_url + "'><p>Original Post</p></a>"
 				}
 			}
 		}
